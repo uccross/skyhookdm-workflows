ea6f3d24cfbd14e8e94bb9d93fcb6f4257b326fe
Jayjeet Chakraborty <jc.github@rediffmail.com>
2020-06-10 13:45:12 +0530

    remove results dir from git and add visualization workflow

--- rook/ceph-client/entrypoint.sh
+++ rook/ceph-client/entrypoint.sh
@@ -2,8 +2,9 @@
 set -eux
 
 # run the benchmarks here
-sleep 15
+sleep 2
 ceph osd pool create testbench 100 100
-rados bench -p testbench 10 write --no-cleanup --format=json-pretty > /workspace/write.json
-rados bench -p testbench 10 seq --no-cleanup --format=json-pretty > /workspace/seq.json
-rados bench -p testbench 10 rand --no-cleanup --format=json-pretty > /workspace/rand.json
+rados bench -p testbench 120 write --no-cleanup --format=json-pretty > /workspace/write.json
+rados bench -p testbench 120 seq --no-cleanup --format=json-pretty > /workspace/seq.json
+rados bench -p testbench 120 rand --no-cleanup --format=json-pretty > /workspace/rand.json
+rados -p testbench cleanup
