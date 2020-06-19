554ca1b70d1bb6670ef6a69e25918b7dbb841b09
Jayjeet Chakraborty <jc.github@rediffmail.com>
2020-06-19 18:17:43 +0530

    wip: update to read and write tests for blockdevices

--- rook/dd/dd-image/entrypoint.sh
+++ rook/dd/dd-image/entrypoint.sh
@@ -6,7 +6,8 @@ blockdevices=($BLOCKDEVICES)
 i=0
 for blkdev in "${blockdevices[@]}"    
 do
-dd if=/dev/$blkdev of=here bs=1G count=1 oflag=direct 2> "/tmp/dd-${blkdev}"
+dd if=/dev/zero of=/dev/$blkdev bs=1G count=1 oflag=direct 2> "/tmp/dd-write-${blkdev}"
+dd if=/dev/$blkdev of=/dev/null bs=1G count=1 oflag=nocache 2> "/tmp/dd-read-${blkdev}"
 i=$((i+1))
 done
 
