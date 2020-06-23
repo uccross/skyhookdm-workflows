bf28ef074f3643472c51b7c5d923b3390ba167c2
Jayjeet Chakraborty <jc.github@rediffmail.com>
2020-06-23 15:28:01 +0530

    wip: minor changes

--- rook/kubestone-iperf/setup.sh
+++ rook/kubestone-iperf/setup.sh
@@ -5,4 +5,3 @@ export KUBECONFIG=./kubeconfig/config
 
 # start the iperf benchmark pods in kubestone namespace
 kubectl create -n kubestone -f ./kubestone/config/samples/perf_v1alpha1_iperf3.yaml
-
--- rook/kubestone-iperf/teardown.sh
+++ rook/kubestone-iperf/teardown.sh
@@ -3,6 +3,5 @@ set -ex
 
 export KUBECONFIG=./kubeconfig/config
 
-# delete iperf3 tests
+# delete iperf3 test pods
 kubectl delete -n kubestone -f ./kubestone/config/samples/perf_v1alpha1_iperf3.yaml
-
