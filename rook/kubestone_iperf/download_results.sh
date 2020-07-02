107fe6a71110c9a4e1cd1df302dc1feb9e1a6768
Jayjeet Chakraborty <jc.github@rediffmail.com>
2020-07-03 03:17:40 +0530

    add timeout to iperf workflow

---
+++ rook/kubestone_iperf/download_results.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -eu
+
+count=0
+while [ "$(kubectl get pod -n kubestone -l iperf-mode=client -o jsonpath="{.items[0].status.phase}")" != "Succeeded" ]; 
+do 
+if [ $count == 30 ]; then
+    exit 1
+fi
+echo "waiting for client pod to complete";
+sleep 5
+count=$(( count + 1 ))
+done
+
+pod=$(kubectl get pod -n kubestone -l iperf-mode=server -o jsonpath="{.items[0].metadata.name}")
+kubectl logs -n kubestone $pod > ./kubestone_iperf/results
