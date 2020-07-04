4af930df4977a4f37a244017f50eba569f9cf76c
Jayjeet Chakraborty <jc.github@rediffmail.com>
2020-07-04 06:15:21 +0530

    add namespace environment var to ceph benchmark workflow and make workflows synchronous (#31)
    * make ceph benchmarks workflow synchronous
    * make iperf workflow synchronous
    * update readme
    * add namespace env var to ceph benchmarks workflow
    * extract copy-config to a separate script and fix the workflows
    * add timeout to iperf workflow
    * collect results from client since we would eventually need to scale out clients to different nodes
    
    fixes #26 
    fixes #29

--- rook/ceph_benchmarks/download_results.sh
+++ rook/ceph_benchmarks/download_results.sh
@@ -3,14 +3,13 @@ set -eu
 
 mkdir -p ./ceph_benchmarks/results
 
-export KUBECONFIG=./kubeconfig/config
-POD=$(kubectl get pod -l app=ceph-benchmarks -o jsonpath="{.items[0].metadata.name}")
+pod=$(kubectl get pod -n "$NAMESPACE" -l app=ceph-benchmarks -o jsonpath="{.items[0].metadata.name}")
 
-kubectl exec "$POD" -- cat /tmp/write.json > ./ceph_benchmarks/results/write.json
-kubectl exec "$POD" -- cat /tmp/seq.json > ./ceph_benchmarks/results/seq.json
-kubectl exec "$POD" -- cat /tmp/rand.json > ./ceph_benchmarks/results/rand.json
+kubectl exec -n "$NAMESPACE" "$pod" -- cat /tmp/write.json > ./ceph_benchmarks/results/write.json
+kubectl exec -n "$NAMESPACE" "$pod" -- cat /tmp/seq.json > ./ceph_benchmarks/results/seq.json
+kubectl exec -n "$NAMESPACE" "$pod" -- cat /tmp/rand.json > ./ceph_benchmarks/results/rand.json
 
-OSD_COUNT=$(kubectl exec "$POD" -- ls -f /tmp/ | grep -c osd)
-for (( i=0; i<$OSD_COUNT; i++ )); do
-    kubectl exec "$POD" -- cat /tmp/osd.$i.json > ./ceph_benchmarks/results/osd.$i.json
+osd_count=$(kubectl exec -n "$NAMESPACE" "$pod" -- ls -f /tmp/ | grep -c osd)
+for (( i=0; i<$osd_count; i++ )); do
+    kubectl exec -n "$NAMESPACE" "$pod" -- cat /tmp/osd.$i.json > ./ceph_benchmarks/results/osd.$i.json
 done
