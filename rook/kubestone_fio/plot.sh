af5603feb33a56e22f8304e3370e2693437afa7b
Jayjeet Chakraborty <jc.github@rediffmail.com>
2020-07-07 15:36:05 +0530

    fix plot script

--- rook/kubestone_fio/plot.sh
+++ rook/kubestone_fio/plot.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 set -eu
 
+for blkdev in ${BLOCKDEVICES[@]};
+do
 for io_depth in ${IO_DEPTH[@]};
 do
-/fio_plot -i ./kubestone_fio/results/FIO_OUTPUT/sda2/4m  -T fio-benchmarks -g -r randread -r randwrite -r read -r write -t bw iops lat -d "$io_depth" -n "$NUM_JOBS"
+/fio_plot -i ./kubestone_fio/results/FIO_OUTPUT/$blkdev/$BLOCKSIZE  -T fio-line-$blkdev -g -r randread -r randwrite -r read -r write -t bw iops lat -d "$io_depth" -n "$NUM_JOBS"
+done
+/fio_plot -i ./kubestone_fio/results/FIO_OUTPUT/$blkdev/$BLOCKSIZE  -T fio-3d-$blkdev -r randread -t iops -L
+/fio_plot -i ./kubestone_fio/results/FIO_OUTPUT/$blkdev/$BLOCKSIZE  -T fio-3d-$blkdev -r randwrite -t iops -L
+/fio_plot -i ./kubestone_fio/results/FIO_OUTPUT/$blkdev/$BLOCKSIZE  -T fio-3d-$blkdev -r read -t iops -L
+/fio_plot -i ./kubestone_fio/results/FIO_OUTPUT/$blkdev/$BLOCKSIZE  -T fio-3d-$blkdev -r write -t iops -L
 done
 
-plots=$(ls | grep "fio-benchmarks-*")
+plots=$(find . -maxdepth 1 -name "*.png")
 for plot in ${plots[@]};
 do
 mv $plot ./kubestone_fio/results
