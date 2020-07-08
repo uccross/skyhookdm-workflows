f55dba3e79c310ef01effef47323bf62afd15207
Jayjeet Chakraborty <jc.github@rediffmail.com>
2020-07-08 17:32:01 +0530

    run ceph benchmarks for multiple io depths

--- rook/ceph_benchmarks/download_results.sh
+++ rook/ceph_benchmarks/download_results.sh
@@ -5,9 +5,12 @@ mkdir -p ./ceph_benchmarks/results
 
 pod=$(kubectl get pod -n "$NAMESPACE" -l app=ceph-benchmarks -o jsonpath="{.items[0].metadata.name}")
 
-kubectl exec -n "$NAMESPACE" "$pod" -- cat /tmp/write.json > ./ceph_benchmarks/results/write.json
-kubectl exec -n "$NAMESPACE" "$pod" -- cat /tmp/seq.json > ./ceph_benchmarks/results/seq.json
-kubectl exec -n "$NAMESPACE" "$pod" -- cat /tmp/rand.json > ./ceph_benchmarks/results/rand.json
+for io_depth in ${IO_DEPTH{@}};
+do
+kubectl exec -n "$NAMESPACE" "$pod" -- cat /tmp/write.json > ./ceph_benchmarks/results/write-$io_depth.json
+kubectl exec -n "$NAMESPACE" "$pod" -- cat /tmp/seq.json   > ./ceph_benchmarks/results/seq-$io_depth.json
+kubectl exec -n "$NAMESPACE" "$pod" -- cat /tmp/rand.json  > ./ceph_benchmarks/results/rand-$io_depth.json
+done
 
 osd_count=$(kubectl exec -n "$NAMESPACE" "$pod" -- ls -f /tmp/ | grep -c osd)
 for (( i=0; i<$osd_count; i++ )); do
--- rook/ceph_benchmarks/run_benchmarks.sh
+++ rook/ceph_benchmarks/run_benchmarks.sh
@@ -2,28 +2,32 @@
 set -eu
 
 # rados benchmarks
-
 # remove existing pool, if any
 ceph osd pool rm testbench testbench --yes-i-really-really-mean-it
 
 # create a pool with 100 PGs
 ceph osd pool create testbench 128 128
 
-rados bench -p testbench 120 write --no-cleanup --format=json-pretty > /tmp/write.json
-rados bench -p testbench 120 seq --no-cleanup --format=json-pretty > /tmp/seq.json
-rados bench -p testbench 120 rand --no-cleanup --format=json-pretty > /tmp/rand.json
+for io_depth in ${IO_DEPTH{@}};
+do
+rados bench --no-hints --concurrent-ios $io_depth -p testbench 120 write --no-cleanup --format=json-pretty > /tmp/write-$io_depth.json
+rados bench --no-hints --concurrent-ios $io_depth -p testbench 120 seq   --no-cleanup --format=json-pretty > /tmp/seq-$io_depth.json
+rados bench --no-hints --concurrent-ios $io_depth -p testbench 120 rand  --no-cleanup --format=json-pretty > /tmp/rand-$io_depth.json
+done
 
 # clean up and delete the pool
 rados -p testbench cleanup
 ceph osd pool rm testbench testbench --yes-i-really-really-mean-it
 
 # clean the rados benchmark output files
-sed -i '1d' /tmp/write.json
-sed -i '1d' /tmp/seq.json
-sed -i '1d' /tmp/rand.json
+for io_depth in ${IO_DEPTH{@}};
+do
+sed -i '1d' /tmp/write-$io_depth.json
+sed -i '1d' /tmp/seq-$io_depth.json
+sed -i '1d' /tmp/rand-$io_depth.json
+done
 
 # osd benchmarks
-
 # count the osds
 osd_count=$(ceph osd ls | wc -l)
 
