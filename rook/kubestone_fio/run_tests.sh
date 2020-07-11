50fa2e0982c3ddcd71491cf06a3d0d360f012e41
Jayjeet Chakraborty <jc.github@rediffmail.com>
2020-07-11 18:49:20 +0530

    update fio script to format the partition before use

--- rook/kubestone_fio/run_tests.sh
+++ rook/kubestone_fio/run_tests.sh
@@ -6,15 +6,18 @@ export PYTHONIOENCODING=UTF-8
 pod=$(kubectl get pod -n kubestone -l app=fio-benchmarks -o jsonpath="{.items[0].metadata.name}")
 while [ "$(kubectl get pod -n kubestone -l app=fio-benchmarks -o jsonpath="{.items[0].status.phase}")" != "Running" ]
 do
+echo "waiting for pod to comeup"
 sleep 5
 done
 
 targets=""
 for blkdev in ${BLOCKDEVICES[@]};
 do
+# format the partition
+kubectl exec -n kubestone $pod -- mkfs.ext4 /dev/$blkdev
 targets+=" /dev/$blkdev "
 done
 
 bench_fio_cmd="bench_fio --target ${targets} --template /fio-plot/benchmark_script/fio-job-template.fio --type device --mode read write randread randwrite --output FIO_OUTPUT -e $IO_ENGINE -b $BLOCKSIZE -s $SIZE --iodepth $IO_DEPTH --numjobs $NUM_JOBS"
-
+# run the benchmark
 kubectl exec -n kubestone $pod  -- $bench_fio_cmd
