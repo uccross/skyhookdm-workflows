289e72617a62c21d9cfc70cbe500a36636f9af17
Jayjeet Chakraborty <jc.github@rediffmail.com>
2020-07-13 22:34:19 +0530

    update plot script

--- rook/kubestone_iperf/plot.py
+++ rook/kubestone_iperf/plot.py
@@ -6,11 +6,8 @@ results_dir = './kubestone_iperf/results'
 
 files = os.listdir(results_dir)
 
-legend = []
-
 for file in files:
     if not file.endswith('.json'): continue
-    legend.append(file[:-5])
     with open(os.path.join(results_dir, file)) as f:
         results = json.load(f)
 
@@ -21,10 +18,10 @@ for file in files:
         seconds.append(float(run["sum"]["end"]))
         bandwidth.append(float(run["sum"]["bits_per_second"])/(1000*1000))
 
-    plt.plot(seconds, bandwidth, markersize=10, linewidth=3.0)
+    plt.plot(seconds, bandwidth, markersize=10, linewidth=3.0, label=file[:-5])
 
 plt.xlabel('time (in seconds)')
 plt.ylabel('bandwidth (mb/s)')
-plt.title('iperf benchmarks')
-plt.legend(legend)
+plt.title('IPERF3 Benchmarks  (client side)')
+plt.legend()
 plt.savefig(os.path.join(results_dir, './iperf-benchmarks.png'), dpi=300, bbox_inches='tight')
