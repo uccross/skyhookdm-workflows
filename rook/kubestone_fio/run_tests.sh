d3d4a06ee80cd5f2431b841d0c7ddf9d978a30b7
Jayjeet Chakraborty <jc.github@rediffmail.com>
2020-08-01 21:49:58 +0530

    updates to fio and iperf benchmarks (#40)
    * misc updates
    
    * update rook.yml
    
    * updates to fio benchmarks

--- rook/kubestone_fio/run_tests.sh
+++ rook/kubestone_fio/run_tests.sh
@@ -14,10 +14,12 @@ targets=""
 for blkdev in ${BLOCKDEVICES[@]};
 do
 # format the partition
-kubectl exec -n kubestone $pod -- mkfs.ext4 /dev/$blkdev
+kubectl exec -n kubestone "$pod" -- mkfs.ext4 /dev/$blkdev
 targets+=" /dev/$blkdev "
 done
 
-bench_fio_cmd="bench_fio --target ${targets} --template /fio-plot/benchmark_script/fio-job-template.fio --type device --mode read write randread randwrite --output FIO_OUTPUT -e $IO_ENGINE -b $BLOCKSIZE -s $SIZE --iodepth $IO_DEPTH --numjobs $NUM_JOBS"
+kubectl exec -n kubestone "$pod" -- sh -c "echo 3 | tee /proc/sys/vm/drop_caches"
+
+bench_fio_cmd="bench_fio --target ${targets} --template /fio-plot/benchmark_script/fio-job-template.fio --type device --mode read write randread randwrite --output FIO_OUTPUT -e ${IO_ENGINE} -b ${BLOCKSIZE} -s ${SIZE} --iodepth ${IO_DEPTH} --numjobs ${NUM_JOBS}"
 # run the benchmark
-kubectl exec -n kubestone $pod  -- $bench_fio_cmd
+kubectl exec -n kubestone "$pod"  -- ${bench_fio_cmd}
