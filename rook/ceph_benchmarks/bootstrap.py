12fc48c4c83f6b6c917bec56d3f7ece20acfe6bd
Jayjeet Chakraborty <jc.github@rediffmail.com>
2020-08-04 05:43:30 +0530

    add step to generate deployment file manually

---
+++ rook/ceph_benchmarks/bootstrap.py
@@ -0,0 +1,32 @@
+import os
+import yaml
+
+
+config = {
+    "apiVersion": "v1",
+    "kind": "Pod",
+    "metadata": {
+        "name": "ceph-benchmarks",
+        "labels": {
+            "app": "ceph-benchmarks"
+        }
+    },
+    "spec": {
+        "restartPolicy": "Never",
+        "containers": [
+            {
+                "name": "ceph-benchmarks",
+                "image": "uccross/skyhookdm-ceph:v14.2.9",
+                "command": ['sleep', 'infinity']
+            }
+        ]
+    }
+}
+
+if __name__ == "__main__":
+    if os.environ.get('CLIENT_NODE', None):
+        config["spec"]["nodeSelector"] = {
+            "kubernetes.io/hostname": os.environ["CLIENT_NODE"]
+        }
+    with open(os.path.join('./ceph_benchmarks/deployment.yaml'), 'w') as f:
+        f.write(yaml.dump(config))
