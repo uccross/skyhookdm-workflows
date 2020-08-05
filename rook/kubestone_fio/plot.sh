eb6c51b7d3c5faced9f4d81639389e2cb5c51d8a
Jayjeet Chakraborty <jc.github@rediffmail.com>
2020-08-05 15:33:10 +0530

    update fio scripts

--- rook/kubestone_fio/plot.sh
+++ rook/kubestone_fio/plot.sh
@@ -5,15 +5,15 @@ for blkdev in ${BLOCKDEVICES[@]};
 do
 for io_depth in ${IO_DEPTH[@]};
 do
-fio_plot -i ./kubestone_fio/results/FIO_OUTPUT/$blkdev/$BLOCKSIZE  -T fio-randread-$io_depth-$blkdev  -g -r randread  -t bw iops lat -d "$io_depth" -n "$NUM_JOBS"
-fio_plot -i ./kubestone_fio/results/FIO_OUTPUT/$blkdev/$BLOCKSIZE  -T fio-randwrite-$io_depth-$blkdev -g -r randwrite -t bw iops lat -d "$io_depth" -n "$NUM_JOBS"
-fio_plot -i ./kubestone_fio/results/FIO_OUTPUT/$blkdev/$BLOCKSIZE  -T fio-read-$io_depth-$blkdev      -g -r read      -t bw iops lat -d "$io_depth" -n "$NUM_JOBS"
-fio_plot -i ./kubestone_fio/results/FIO_OUTPUT/$blkdev/$BLOCKSIZE  -T fio-write-$io_depth-$blkdev     -g -r write     -t bw iops lat -d "$io_depth" -n "$NUM_JOBS"
+fio_plot -i ./kubestone_fio/results/FIO_OUTPUT/${blkdev}/${BLOCKSIZE}  -T fio-randread-${io_depth}-${blkdev}  -g --rw randread  -t bw iops lat -d ${io_depth} -n ${NUM_JOBS}
+fio_plot -i ./kubestone_fio/results/FIO_OUTPUT/${blkdev}/${BLOCKSIZE}  -T fio-randwrite-${io_depth}-${blkdev} -g --rw randwrite -t bw iops lat -d ${io_depth} -n ${NUM_JOBS}
+fio_plot -i ./kubestone_fio/results/FIO_OUTPUT/${blkdev}/${BLOCKSIZE}  -T fio-read-${io_depth}-${blkdev}      -g --rw read      -t bw iops lat -d ${io_depth} -n ${NUM_JOBS}
+fio_plot -i ./kubestone_fio/results/FIO_OUTPUT/${blkdev}/${BLOCKSIZE}  -T fio-write-${io_depth}-${blkdev}     -g --rw write     -t bw iops lat -d ${io_depth} -n ${NUM_JOBS}
 done
 done
 
 plots=$(find . -maxdepth 1 -name "*.png")
 for plot in ${plots[@]};
 do
-mv $plot ./kubestone_fio/results
+mv ${plot} ./kubestone_fio/results
 done
