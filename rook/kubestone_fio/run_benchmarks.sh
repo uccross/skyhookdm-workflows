e3a462cb2d6b5a106f33bebbacecf3b0e992364e
Jayjeet Chakraborty <jc.github@rediffmail.com>
2020-08-21 16:29:28 +0530

    update fios run benchmark script and workflow

--- rook/kubestone_fio/run_benchmarks.sh
+++ rook/kubestone_fio/run_benchmarks.sh
@@ -1,30 +1,26 @@
 #!/bin/bash
 set -eu
 
-function info {
-  echo "[info] - $(date) - $@"
-}
-
 export PYTHONIOENCODING=UTF-8
 
 pod=$(kubectl get pod -n kubestone -l app=fio-benchmarks -o jsonpath="{.items[0].metadata.name}")
 while [ "$(kubectl get pod -n kubestone -l app=fio-benchmarks -o jsonpath="{.items[0].status.phase}")" != "Running" ]
 do
-info "waiting for pod to comeup"
+echo "[INFO] Waiting for pod to comeup..."
 sleep 5
 done
 
 targets=""
 for blkdev in ${BLOCKDEVICES[@]};
 do
-info "formatting /dev/${blkdev}"
+echo "[INFO] Formatting /dev/${blkdev}..."
 kubectl exec -n kubestone ${pod} -- mkfs.ext4 /dev/${blkdev}
 targets+=" /dev/${blkdev} "
 done
 
-info "dropping caches"
+echo "[INFO] Dropping system caches..."
 kubectl exec -n kubestone ${pod} -- sh -c "echo 3 | tee /proc/sys/vm/drop_caches"
-bench_fio_cmd="bench_fio --target ${targets} --template /fio-plot/benchmark_script/fio-job-template.fio --type device --mode ${MODES} --output FIO_OUTPUT -e ${IO_ENGINE} -b ${BLOCKSIZE} --iodepth ${IO_DEPTH} --numjobs ${NUM_JOBS}"
 
-info "running benchmark"
-kubectl exec -n kubestone ${pod}  -- ${bench_fio_cmd}
+echo "[INFO] Running fio benchmark..."
+cmd="bench_fio --target ${targets} --template /fio-plot/benchmark_script/fio-job-template.fio --type device --mode ${MODES} --output FIO_OUTPUT -e ${IO_ENGINE} -b ${BLOCKSIZE} --iodepth ${IO_DEPTH} --numjobs ${NUM_JOBS}"
+kubectl exec -n kubestone ${pod}  -- ${cmd}
