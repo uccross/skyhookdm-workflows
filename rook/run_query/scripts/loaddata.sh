ce543b893eaed40a51103aaec9694d5fbc2512a0
Jayjeet Chakraborty <jc.github@rediffmail.com>
2020-09-01 17:09:17 +0530

    add multithreaded put script

--- rook/run_query/scripts/loaddata.sh
+++ rook/run_query/scripts/loaddata.sh
@@ -6,22 +6,22 @@ function k8s_exec {
   kubectl -n "$NAMESPACE" exec "$pod" -- "$@"
 }
 
-# download the rados-store-glob.sh script
-k8s_exec curl https://raw.githubusercontent.com/uccross/skyhookdm-ceph/skyhook-luminous/src/progly/rados-store-glob.sh --output rados-store-glob.sh
-k8s_exec chmod +x ./rados-store-glob.sh
-
 # delete any zombie pool and create a new pool
+echo "[INFO] Delete existing pool and create a new pool..."
 k8s_exec ceph osd pool delete tpchdata tpchdata --yes-i-really-really-mean-it
 k8s_exec ceph osd pool create tpchdata "$PG_COUNT" "$PG_COUNT" "$POOLTYPE"
 
 # set pool size to 1 to disable replicacy
+echo "[INFO] Disable replicacy on pool..."
 k8s_exec ceph osd pool set tpchdata size 1
 
 # download flatbuffer 10MB lineitem dataset
+echo "[INFO] Download dataset..."
 k8s_exec curl https://users.soe.ucsc.edu/~jlefevre/skyhookdb/testdata/pdsw19/sampledata/fbx.lineitem.10MB.75Krows.obj.0 --output fbx.lineitem.10MB.75Krows.obj.0
 
-# put the data in a loop
-for (( k=1; k<=REPLICACY_COUNT; k++ ))
-do
-    k8s_exec rados -p tpchdata put public.lineitem."$k" fbx.lineitem.10MB.75Krows.obj.0
-done
+# put the data into objects in a loop using all the cores
+echo "[INFO] Copying put script to pod..."
+kubectl cp ./run_query/scripts/put.py "${NAMESPACE}/${pod}:/tmp"
+
+echo "[INFO] Launching multithreaded put..."
+k8s_exec python3 /tmp/put.py --count "$OBJECT_COUNT"
