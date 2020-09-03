0cad91b6d7e762aa77b104283cbbc1a7335c5bb4
Jayjeet Chakraborty <jc.github@rediffmail.com>
2020-09-04 00:53:27 +0530

    format geni scripts with black

--- rook/geni/setup-nodes.py
+++ rook/geni/setup-nodes.py
@@ -1,7 +1,7 @@
 import os
 
 # install packages
-os.system('pip install PyYAML xmltodict')
+os.system("pip install PyYAML xmltodict")
 
 import yaml
 import xmltodict
@@ -15,35 +15,36 @@ from geni.aggregate import cloudlab
 request = portal.context.makeRequestRSpec()
 
 node_list = list()
-node_count = int(os.environ['NODE_COUNT'])
+node_count = int(os.environ["NODE_COUNT"])
 
-slice_name = str(os.environ['NODE_SLICE'])
+slice_name = str(os.environ["NODE_SLICE"])
 
 # Create the raw "PC" nodes
 for _idx in range(0, node_count):
-    node = request.RawPC('node_{}'.format(_idx))
-    node.hardware_type = str(os.environ['NODE_HARDWARE'])
+    node = request.RawPC("node_{}".format(_idx))
+    node.hardware_type = str(os.environ["NODE_HARDWARE"])
     node_list.append(node)
 
 # Create a link between them
 link1 = request.Link(members=node_list)
 
 # load context
-ctx = util.loadContext(path="/geni-context.json",
-                       key_passphrase=os.environ['GENI_KEY_PASSPHRASE'])
+ctx = util.loadContext(
+    path="/geni-context.json", key_passphrase=os.environ["GENI_KEY_PASSPHRASE"]
+)
 
 # create slice
 util.createSlice(ctx, slice_name, 300, True)
 
 # identify the cluster
-cluster = str(os.environ['NODE_CLUSTER'])
-if cluster == 'utah':
+cluster = str(os.environ["NODE_CLUSTER"])
+if cluster == "utah":
     cluster = cloudlab.Utah
 
-elif cluster == 'wisconsin':
+elif cluster == "wisconsin":
     cluster = cloudlab.Wisconsin
 
-elif cluster == 'clemson':
+elif cluster == "clemson":
     cluster = cloudlab.Clemson
 
 # create sliver
@@ -53,8 +54,8 @@ dict_response = xmltodict.parse(manifest.text)
 
 node_ip_list = list()
 
-for idx in range(0, len(dict_response['rspec']['node'])):
-    node_ip_list.append(str(dict_response['rspec']['node'][idx]['host']['@ipv4']))
+for idx in range(0, len(dict_response["rspec"]["node"])):
+    node_ip_list.append(str(dict_response["rspec"]["node"][idx]["host"]["@ipv4"]))
 
-with open('./geni/hosts', 'w') as f:
-    f.write(' '.join(node_ip_list))
+with open("./geni/hosts", "w") as f:
+    f.write(" ".join(node_ip_list))
