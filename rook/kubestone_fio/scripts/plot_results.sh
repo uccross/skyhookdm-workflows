003be50b9e16d346192e235225f3da4c63a42439
Jayjeet Chakraborty <jc.github@rediffmail.com>
2020-09-08 02:07:53 +0530

    add back fio-plot (#54)
    * update plot for iperf
    
    * fix and add back fio-plot
    
    * change BLOCKSIZES to BLOCKSIZE
    
    * pin the image

---
+++ rook/kubestone_fio/scripts/plot_results.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -eu
+
+for blkdev in ${BLOCKDEVICES[@]};
+do
+for io_depth in ${IO_DEPTH[@]};
+do
+for blksize in ${BLOCKSIZE[@]};
+do
+for jobsize in ${NUM_JOBS[@]};
+do
+fio_plot -i "./kubestone_fio/results/${blkdev}/${blksize}"  -T "fio-randread-${io_depth}-${blkdev}-${blksize}-${jobsize}"  -g -r randread  -t bw iops lat -d "$io_depth" -n "$jobsize"
+fio_plot -i "./kubestone_fio/results/${blkdev}/${blksize}"  -T "fio-randwrite-${io_depth}-${blkdev}-${blksize}-${jobsize}" -g -r randwrite -t bw iops lat -d "$io_depth" -n "$jobsize"
+fio_plot -i "./kubestone_fio/results/${blkdev}/${blksize}"  -T "fio-read-${io_depth}-${blkdev}-${blksize}-${jobsize}"      -g -r read      -t bw iops lat -d "$io_depth" -n "$jobsize"
+fio_plot -i "./kubestone_fio/results/${blkdev}/${blksize}"  -T "fio-write-${io_depth}-${blkdev}-${blksize}-${jobsize}"     -g -r write     -t bw iops lat -d "$io_depth" -n "$jobsize"
+done
+done
+done
+done
+
+plots=$(find . -maxdepth 1 -name "*.png")
+for plot in ${plots[@]};
+do
+mv "$plot" ./kubestone_fio/results
+done
